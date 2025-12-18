34a6e928a08f7dd5c3980b7e04611932f9b50438
Oltean Romeo <65122445+viperh@users.noreply.github.com>
2025-12-18 09:05:55 +0100

    Remove run instructions from main.go
    Removed unnecessary tips for running the code.

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
 func main() {
 	r := gin.New()
 
